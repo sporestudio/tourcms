6b256357a9a06c1de1e7d30a37db3b56b7a8f21c
jorgerodriguez-palisis <jorge.rodriguez@palisis.com>
2025-03-31 13:40:23 +0200

    TOURCMS-11227 fix: add exceptions in router and correct fix in login temp
    - Add exceptions in router to manage the errors in the router class.
    - Fix the action of the login template to point to index with the
      properly params.

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,24 +1,32 @@
 <?php
-    namespace Core;
-    class Router {
-        public function dispatch() {
-            $controller = isset($_GET['controller']) ? ucfirst($_GET['controller']) . 'Controller' : 'LoginController';
-            $action = isset($_GET['action']) ? $_GET['action'] : 'index';
+namespace Core;
 
-            $controllerFile = __DIR__ . '/../controllers/'. $controller .'.php';
+class Router {
+    public function dispatch() {
+        $controller = isset($_GET['controller']) ? ucfirst($_GET['controller']) . 'Controller' : 'LoginController';
+        $action = isset($_GET['action']) ? $_GET['action'] : 'index';
 
-            if (file_exists($controllerFile)) {
-                require_once $controllerFile;
+        $controllerClass = "\\Controller\\" . $controller;
+        $controllerFile = __DIR__ . '/../controllers/' . $controller . '.php';
 
-                $controllerInstance = new $controller();
-                if(method_exists($controllerInstance, $action)) {
+        if (file_exists($controllerFile)) {
+            require_once $controllerFile;
+
+            
+            if (class_exists($controllerClass)) {
+                $controllerInstance = new $controllerClass();
+
+                if (method_exists($controllerInstance, $action)) {
                     return $controllerInstance->$action();
                 } else {
-                    echo "Error: Action not find.";
+                    throw new \Exception("Action '$action' not found in controller '$controllerClass'.");
                 }
             } else {
-                echo "Error: controller not found.";
+                throw new \Exception("Controller class '$controllerClass' not found.");
             }
+        } else {
+            throw new \Exception("Controller file '$controllerFile' not found.");
         }
     }
+}
 ?>
