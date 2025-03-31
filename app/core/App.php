15d903ee47c28d0b76aba212320c94cbd2429c46
jorgerodriguez-palisis <jorge.rodriguez@palisis.com>
2025-03-31 11:40:49 +0200

    TOURCMS-11227 feat: add app core util
    Add app class to handle the configuration and routing in a centralized
    way through app instance.

---
+++ app/core/App.php
@@ -0,0 +1,22 @@
+<?php
+    namespace Core;
+    use Core\Router;
+
+    class App {
+        protected $config;
+        protected $router;
+
+        public function __construct() {
+           
+            global $config;
+
+            $this->config = $config;
+
+            $this->router = new Router();
+        }
+
+        public function run() {
+            $this->router->dispatch();
+        }
+    }
+?>
