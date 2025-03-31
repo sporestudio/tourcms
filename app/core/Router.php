f6e305ad37924015039ecd38a33501107e4eeebe
jorgerodriguez-palisis <jorge.rodriguez@palisis.com>
2025-03-31 11:38:43 +0200

    TOURCMS-11227 refactor: change router filename
    Change router filename to comply with good practices with camelcase used
    in php.

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,15 +1,18 @@
 <?php
+    namespace Core;
     class Router {
         public function dispatch() {
-            $controller = isset($_GET['controller']) ? $_GET['controller'] . 'Controller' : 'loginController';
+            $controller = isset($_GET['controller']) ? ucfirst($_GET['controller']) . 'Controller' : 'LoginController';
             $action = isset($_GET['action']) ? $_GET['action'] : 'index';
 
             $controllerFile = __DIR__ . '/../controllers/'. $controller .'.php';
+
             if (file_exists($controllerFile)) {
                 require_once $controllerFile;
+
                 $controllerInstance = new $controller();
                 if(method_exists($controllerInstance, $action)) {
-
+                    return $controllerInstance->$action();
                 } else {
                     echo "Error: Action not find.";
                 }
