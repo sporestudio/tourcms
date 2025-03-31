78d4cd0da10569bf10c4769c8f7159ecb9bd279e
jorgerodriguez-palisis <jorge.rodriguez@palisis.com>
2025-03-31 14:13:14 +0200

    TOURCMS-11227 feat: add session middleware to handle create/expire session
    Create a middleware that is used to manage and validate user sessions in the app. Its main purpose is to ensure that only authenticated users can access protected paths, while unauthenticated users are redirected to the login page.

---
+++ app/middleware/SessionMiddleware.php
@@ -0,0 +1,60 @@
+<?php
+namespace Middleware;
+
+use Lib\RedisService;
+use Controller\RedisController;
+
+class SessionMiddleware {
+    protected $redis;
+    protected $exTime = 600;
+
+    public function __construct() {
+        global $REDIS_HOST, $REDIS_PORT, $REDIS_PASSWORD;
+
+        $this->redis = RedisController::getInstance($REDIS_HOST, $REDIS_PORT, $REDIS_PASSWORD);
+        session_start();
+    }
+
+    public function handle() {
+        // Excluir rutas específicas de la validación de sesión
+        $excludedRoutes = [
+            'login/index',
+            'login/process'
+        ];
+
+        $currentRoute = strtolower($_GET['controller'] ?? 'login') . '/' . strtolower($_GET['action'] ?? 'index');
+
+        if (in_array($currentRoute, $excludedRoutes)) {
+            return; // No validar la sesión en estas rutas
+        }
+
+        if (!isset($_SESSION['user'])) {
+            $this->redirectLogin();
+        }
+
+        $userKey = 'LOGIN' . $_SESSION['user'];
+        $loginJSON = $this->redis->getItemFromRedis($userKey, RedisService::REDIS_TYPE_STRING);
+
+        if (!$loginJSON) {
+            $this->redirectLogin();
+        }
+
+        $loginData = json_decode($loginJSON, true);
+        if (!isset($loginData['ttl']) || time() > $loginData['ttl']) {
+            session_destroy();
+            $this->redirectLogin();
+        }
+
+        // Renovar TTL de la sesión
+        $newTtl = time() + $this->exTime;
+        $loginData['ttl'] = $newTtl;
+        $this->redis->storeItemInRedis($userKey, json_encode($loginData), RedisService::REDIS_TYPE_STRING);
+        $this->redis->expireAt($userKey, $newTtl);
+    }
+
+    private function redirectLogin() {
+        header("Location: /index.php?controller=login&action=index");
+        exit;
+    }
+}
+?>
