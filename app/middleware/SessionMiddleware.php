fa70b60fabc6b90eb091e6ca5082ab812d2f18a9
jorgerodriguez-palisis <jorge.rodriguez@palisis.com>
2025-04-02 15:17:04 +0200

    TOURCMS-11297 refactor: modify middleware to PSR-12 standard

--- app/middleware/SessionMiddleware.php
+++ app/middleware/SessionMiddleware.php
@@ -4,18 +4,22 @@ namespace Middleware;
 use Lib\RedisService;
 use Controller\RedisController;
 
-class SessionMiddleware {
+class SessionMiddleware 
+{
     protected $redis;
     protected $exTime = 500;
 
-    public function __construct() {
+    // TODO: pass to the construct redis as an object.
+    public function __construct() 
+    {
         global $REDIS_HOST, $REDIS_PORT, $REDIS_PASSWORD;
 
         $this->redis = RedisController::getInstance($REDIS_HOST, $REDIS_PORT, $REDIS_PASSWORD);
         session_start();
     }
 
-    public function handle() {
+    public function handle() 
+    {
         $excludedRoutes = [
             'login/index',
             'login/process'
@@ -23,37 +27,32 @@ class SessionMiddleware {
 
         $currentRoute = strtolower($_GET['controller'] ?? 'login') . '/' . strtolower($_GET['action'] ?? 'index');
 
-        if (in_array($currentRoute, $excludedRoutes)) {
+        if (in_array($currentRoute, $excludedRoutes)) 
+        {
             return;
         }
 
-        if (!isset($_SESSION['user'])) {
-            $this->redirectLogin();
-        }
-
-        $userKey = 'LOGIN' . $_SESSION['user'];
-        $loginJSON = $this->redis->getItemFromRedis($userKey, RedisService::REDIS_TYPE_STRING);
-
+        $loginJSON = $this->redis->getItemFromRedis('LOGIN', RedisService::REDIS_TYPE_STRING);
         if (!$loginJSON) {
             $this->redirectLogin();
         }
-
+        
         $loginData = json_decode($loginJSON, true);
         if (!isset($loginData['ttl']) || time() > $loginData['ttl']) {
-            session_destroy();
+            $this->redis->deleteItemFromRedis('LOGIN', RedisService::REDIS_TYPE_STRING);
             $this->redirectLogin();
         }
-
         
+
         $newTtl = time() + $this->exTime;
         $loginData['ttl'] = $newTtl;
-        $this->redis->storeItemInRedis($userKey, json_encode($loginData), RedisService::REDIS_TYPE_STRING);
-        $this->redis->expireAt($userKey, $newTtl);
+        $this->redis->storeItemInRedis('LOGIN', json_encode($loginData), RedisService::REDIS_TYPE_STRING);
+        $this->redis->expireAt('LOGIN', $newTtl);
     }
 
-    private function redirectLogin() {
+    private function redirectLogin() 
+    {
         header("Location: /index.php?controller=login&action=index");
         exit;
     }
 }
-?>
