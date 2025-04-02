5dd776c21025d56e6b73aa3aa6f8bfe871a0f2ab
jorgerodriguez-palisis <jorge.rodriguez@palisis.com>
2025-04-02 15:13:51 +0200

    TOURCMS-11297 refactor: refactorize all controller to comply with PSR-12
    Modify all the controller code to comply the PSR-12 standards code.

--- app/controllers/RedisController.php
+++ app/controllers/RedisController.php
@@ -1,21 +1,23 @@
 <?php
-    namespace Controller;
+namespace Controller;
 
-    use Lib\RedisService;
+use Lib\RedisService;
 
-    class RedisController {
-        private static $instance = null;
-        private $redis;
+class RedisController 
+{
+    private static $instance = null;
+    private $redis;
 
-        private function __construct($host, $port, $passwd) {
-            $this->redis = new RedisService($host, $port, $passwd);
-        }
-
-        public static function getInstance($host, $port, $passwd) {
-            if (self::$instance === null) {
-                self::$instance = new self($host, $port, $passwd);
-            }
-            return self::$instance->redis;
-        }
+    private function __construct($host, $port, $passwd) 
+    {
+        $this->redis = new RedisService($host, $port, $passwd);
     }
-?>
+
+    public static function getInstance($host, $port, $passwd) 
+    {
+        if (self::$instance === null) {
+            self::$instance = new self($host, $port, $passwd);
+        }
+        return self::$instance->redis;
+    }
+}
