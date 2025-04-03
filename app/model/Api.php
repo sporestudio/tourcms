f636a5956ae813ee6b82a708a50f166fb1e130f4
jorgerodriguez-palisis <jorge.rodriguez@palisis.com>
2025-04-03 13:53:39 +0200

    TOURCMS-11251 feat: create api model
    Create a model called API where we encapsulated all the logic to create
    an instance of TourCMS and make api call.

---
+++ app/model/Api.php
@@ -0,0 +1,30 @@
+<?php
+namespace Model;
+
+use TourCMS\Utils\TourCMS as TourCMS;
+
+class Api 
+{
+    private $marketplaceId;
+    private $apiKey;
+    private $resultType;
+    private $timeOut = 0;
+    private $baseUrl;
+
+    public function __construct($marketplaceId, $apiKey, $baseUrl, $timeOut) 
+    {
+        $this->marketplaceId = $marketplaceId;
+        $this->apiKey = $apiKey;
+        $this->baseUrl = $baseUrl;
+        $this->resultType = "simplexml";
+        $this->timeOut = $timeOut;
+    }
+
+
+    public function getTourCMS() 
+    {
+        $tourcms = new TourCMS($this->marketplaceId, $this->apiKey, $this->resultType, $this->timeOut);
+
+        return $tourcms;
+    }
+}
