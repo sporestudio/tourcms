b16a7fde704db47ae0124c41371cbbe2ff82119c
jorgerodriguez-palisis <jorge.rodriguez@palisis.com>
2025-04-03 14:25:30 +0200

    TOURCMS-11251 refactor: pass properly parameters to app instance

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-    require_once __DIR__ . "/config.php";
-    use Core\App;
+require_once __DIR__ ."/vendor/autoload.php";
+require_once __DIR__ . "/config.php";
 
-    $app = new App();
-    $app->run();
-?>
+use Core\App;
+
+$app = new App($MARKETPLACE_ID, $AGENT_API_KEY, $BASE_URL, $TIMEOUT);
+$app->run();
