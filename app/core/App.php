888a96f09448a62e74b5f29b4f9bfb1553018696
jorgerodriguez-palisis <jorge.rodriguez@palisis.com>
2025-04-03 13:47:55 +0200

    TOURCMS-11251 refactor: Redo router and app core util
    Router core changes:
      - Remove unnecessary lines.
      - Add a construct function to pass it the controller factory property.
      - Use the create method from controller factory to create the
        controller instance.
    
    App core changes:
      - Inject dependencies into construct function instead using global
        parameter.
      - Create Controller Factory object to pass it to the router.

--- app/core/App.php
+++ app/core/App.php
@@ -7,14 +7,17 @@ class App
     protected $config;
     protected $router;
 
-    public function __construct() 
-    {
-        
-        global $config;
+    public function __construct($MARKETPLACE_ID, $API_KEY, $BASE_URL, $TIMEOUT) 
+    {  
+        $dependencies = [
+            'MARKETPLACE_ID' => $MARKETPLACE_ID,
+            'API_KEY' => $API_KEY,
+            'BASE_URL' => $BASE_URL,
+            'TIMEOUT' => $TIMEOUT
+        ];
 
-        $this->config = $config;
-
-        $this->router = new Router();
+        $controllerFactory = new ControllerFactory($dependencies);
+        $this->router = new Router($controllerFactory);
     }
 
     public function run() 
