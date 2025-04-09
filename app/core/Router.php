dc1838178ec98b8dad462fdbec0e6e8606fd9a40
jorgerodriguez-palisis <jorge.rodriguez@palisis.com>
2025-04-09 13:25:38 +0200

    TOURCMS-11328 feat: improve routing system in Router
    Changes made in router:
    - Add new function to add the routes (they are defined in the app module) that will be stored in an array.
    - Update dispatch function which it now normalizes the path always
    adding always a / ahead of the path name, and we delete some other
    additional characters that are not necessary, so we ensure a standard
    and solid path.
    - Create a function to redirect to the error page, so in case the path
    doesn't exists it will be redirect to the error page.
    - Instance the template in the constructor to render error page.

--- app/core/Router.php
+++ app/core/Router.php
@@ -2,20 +2,58 @@
 namespace Core;
 
 use Middleware\SessionMiddleware;
+use Core\Template;
+//use Controller\RedisController;
 
 class Router 
 {   
     private $controllerFactory;
+    private $template;
+    private $routes = [];
 
     public function __construct($controllerFactory)
     {
         $this->controllerFactory = $controllerFactory;
+        $this->template = new Template();
     }
+
+    public function addRoutes($path, $controller, $action)
+    {
+        $this->routes[$path] = [
+            'controller' => $controller,
+            'action' => $action,
+        ];
+    }
+
     public function dispatch() 
     {
         $middleware = new sessionMiddleware();
         $middleware->handle();
 
+        $path = $_GET['path'] ?? '/';
+        $normalizedPath = '/' . ltrim($path,'/');
+        error_log("Router: Dispatching path $normalizedPath");
+
+        if (isset($this->routes[$normalizedPath])) {
+            $route = $this->routes[$normalizedPath];
+            $controller = $route['controller'];
+            $controllerInstance = $this->controllerFactory->create($controller);
+            $action = $route['action'];
+
+            error_log("Router: Found route for path $path. Controller: $controller, Action: $action");
+
+            if (method_exists($controllerInstance, $action)) {
+                return $controllerInstance->$action();
+            } else {
+                error_log("Router: Action $action not found in controller $controller");
+                $this->handle404();
+            }
+        } else {
+            error_log("Router: No route found for path $path");
+            $this->handle404();
+        }
+
+        /*
         $controller = isset($_GET['controller']) ? ucfirst($_GET['controller']) . 'Controller' : 'LoginController';
         $action = isset($_GET['action']) ? $_GET['action'] : 'index';
 
@@ -30,5 +68,13 @@ class Router
         } catch (\Exception $e) {
             throw new \Exception($e->getMessage());
         }
+        */
+    }
+
+    private function handle404()
+    {
+        http_response_code(404);
+        echo $this->template->render("404.html", []);
+        exit;
     }
 }
