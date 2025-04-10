e0ec6743bf692ed0fb931f8610932760813fc1ec
jorgerodriguez-palisis <jorge.rodriguez@palisis.com>
2025-04-10 15:15:19 +0200

    TOURCMS-11336-middleware-refactor refactor: Set redis manager as a singleton
    Establish the old Redis controller as Redis Mananger since it is not a
    proper controller but a singleton.

---
+++ app/lib/RedisManager.php
@@ -0,0 +1,31 @@
+<?php
+/*
+    * RedisManager.php
+    * Singleton class to manage Redis connections 
+    * and provide a single instance of RedisService.
+    * Backoffice project.
+*/
+
+namespace Lib;
+
+use Lib\RedisService;
+
+class RedisManager extends RedisService
+{
+    private static $instance = null;
+    protected $redis;
+
+    private function __construct($host, $port, $passwd) 
+    {
+        parent::__construct($host, $port, $passwd);
+        $this->redis = new RedisService($host, $port, $passwd);
+    }
+
+    public static function getInstance($host, $port, $passwd) 
+    {
+        if (self::$instance === null) {
+            self::$instance = new self($host, $port, $passwd);
+        }
+        return self::$instance->redis;
+    }
+}
