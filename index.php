330f320890d8fa01456c0c35625af11a60c0567f
jorgerodriguez-palisis <jorge.rodriguez@palisis.com>
2025-04-14 17:53:54 +0200

    TOURCMS-11336-middleware-refactor: load config.php and pass it to app instance

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 require_once __DIR__ ."/vendor/autoload.php";
-require_once __DIR__ . "/config.php";
+$config = include __DIR__ ."/config.php";
 
 use Core\App;
 
-$app = new App($MARKETPLACE_ID, $AGENT_API_KEY, $BASE_URL, $TIMEOUT);
+$app = new App($config);
 $app->run();
